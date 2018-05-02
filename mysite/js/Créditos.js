aacbd8f7479c6b1a8ccb4a0a3d9f4c818e871274
Mário Azevedo <35400106+MarioAzevedo@users.noreply.github.com>
2018-05-02 01:25:25 +0100

    MySite

---
+++ mysite/js/Créditos.js
@@ -0,0 +1,9 @@
+/*Créditos : TemplateWire
+             John Coates
+             Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+             Bootstrap v3.3.4 (http://getbootstrap.com)
+             jQuery Foundation, Inc. | jquery.org/license
+             Stephane Caron (http://www.no-margin-for-errors.com)
+             David DeSandro / Metafizzy (http://metafizzy.co/#licenses)
+             Balazs Galambosi (maintainer)
+             Michael Herf (Pulse Algorithm)
